5b77baaccf8fdfbd86dc0f3f9ac1d98ca9fe99e8
Peter da Silva <peter.dasilva@flightaware.com>
2026-09-22 15:22:35 +0000

    Update TclInitSTubs

--- generic/tclcasstcl.c
+++ generic/tclcasstcl.c
@@ -41,11 +41,11 @@ Casstcl_Init(Tcl_Interp *interp)
      * This may work with 8.0, but we are using strictly stubs here,
      * which requires 8.1.
      */
-    if (Tcl_InitStubs(interp, "8.1", 0) == NULL) {
+    if (Tcl_InitStubs(interp, "8.1-", 0) == NULL) {
 		return TCL_ERROR;
     }
 
-    if (Tcl_TomMath_InitStubs(interp, "8.5") == NULL) {
+    if (Tcl_TomMath_InitStubs(interp, "8.5-") == NULL) {
 		return TCL_ERROR;
     }
 
